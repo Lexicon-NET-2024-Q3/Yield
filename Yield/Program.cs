1fc831443ec84439304c35902d63d2362dca76b2
Jonathan Krall <jonathan.krall@lexicon.se>
2024-09-24 13:29:56 +0200

    Yield

--- Yield/Program.cs
+++ Yield/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+foreach (int number in GetNumbersOrdinary().Take(10))
+{
+    Console.WriteLine(number);
+}
+
+foreach(int number in GetNumbersYield().Take(10))
+{
+    Console.WriteLine(number);
+}
+
+IEnumerable<int> GetNumbersYield()
+{
+    int i = 0;
+
+    while (true)
+    {
+        yield return ++i; 
+    }
+}
+
+IEnumerable<int> GetNumbersOrdinary()
+{
+    int i = 0;
+    var res = new List<int>();
+
+    while(i < 1000000)
+    {
+        res.Add(++i);
+    }
+
+    return res; 
+}
